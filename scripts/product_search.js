a31f0bc024be6605b2d3313733150e9faf71cce7
Katalina Caldera <katalinacaldera19@gmail.com>
2024-06-10 14:54:23 -0500

    added the second dropdown menu

--- scripts/product_search.js
+++ scripts/product_search.js
@@ -1,22 +1,44 @@
 "use strict"
 window.onload = () => {
-    
+
     let productDropdown = document.querySelector("#productSearch")
     let categoryDropdown = document.querySelector("#category")
+    let categorySelctedDropdown = document.querySelector("#categorySelectedDropdown")
+    productDropdown.addEventListener("change", function () {
+        console.log("product dropdown changed:", this.value);
+        fetchTheDisplayProducts();
+        if (this.value === "category") {
+            categorySelctedDropdown.style.display = "block";
+           fetchCategories();
 
-    productDropdown.addEventListener("change", fetchTheDisplayProducts);
-    categoryDropdown.addEventListener("change", fetchCategories);
+        } else {
+            categorySelctedDropdown.style.display = "none";
+        }
+    });
+    categoryDropdown.addEventListener("change", function(){
+        console.log("Category dropdown changed:", this.value);
+        fetchTheDisplayProducts();
+    });
+    
+       
 }
 
 // grabbing the products from the api
 function fetchTheDisplayProducts() {
-    let selectedCategory = document.querySelector("#category").value;
+    let productTable = document.getElementById("displayAllProducts");
+    productTable.innerHTML = "";
+    let selectedCategory = document.querySelector("#categorySelectedDropdown #category").value;
+    console.log("Selected Category ID:", selectedCategory);
+
     let url;
-    if(selectedCategory){
-        url=`http://localhost:8081/api/categories`;
-    }else{
+    if (selectedCategory && selectedCategory !== "viewAll") {
+
+        url = `http://localhost:8081/api/categories`;
+
+    } else {
         url = `http://localhost:8081/api/products`
     }
+    console.log("Fetch URL:", url); 
 
     fetch(url)
         .then((response) => {
@@ -24,36 +46,47 @@ function fetchTheDisplayProducts() {
                 throw new Error('try again later');
             } return response.json();
         }).then(data => {
-            console.log(data);
-            displayAllProducts(data);
+            
+            // // console.log("API Response:", data);
+            // displayAllProducts(data);
 
         }).catch((err) => {
             console.log('rejected', err);
+            console.log("Product Endpoint URL:", url);
+
         });
 
 
 }
+
+
 // grabing all the categories
 async function fetchCategories() {
+    let categories;
+
     try {
-        
+
         let response = await fetch(`http://localhost:8081/api/categories`)
+        console.log("categories",fetchCategories);
         if (!response.ok) {
             throw new Error(`Cant grab product by category`);
         }
-        let data = await response.json();
-        console.log(data);
-        displayAllCategories(data);
+        categories = await response.json();
+            console.log("categories", categories);
+            displayAllCategories(categories);
+        
     } catch (err) {
         console.log('Error', err);
     }
 
 }
-function displayAllCategories(categories){
-    let categoryDropdown = document.querySelector("#category")
-    categoryDropdown.innerHTML="";
 
-    categories.forEach(category=>{
+function displayAllCategories(categories) {
+    
+    let categoryDropdown = document.querySelector("#categorySelectedDropdown #category");
+    categoryDropdown.innerHTML = "";
+    console.log(categoryDropdown)
+    categories.forEach(category => {
         let option = document.createElement("option");
         option.value = category.categoryId;
         option.textContent = category.name;
